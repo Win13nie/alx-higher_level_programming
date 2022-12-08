468678388e5596599520662215be760397cb90ee
win13nie <wahomewinn00@gmail.com>
2022-12-08 04:46:54 +0300

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) is 0:
-        return None
-    one_loop = True
-    for people in a_dictionary:
-        if one_loop:
-            best = people
-            score = a_dictionary[people]
-            one_loop = False
-        if a_dictionary[people] > score:
-            score = a_dictionary[people]
-            best = people
-    return(best)
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
